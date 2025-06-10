717a6f323c02a154cfc472b0078ddacc56eea14d
Kiran Tang <153486303+GithubRavi74@users.noreply.github.com>
2025-06-10 08:34:50 +0530

    added customer logo on UI

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import streamlit as st
 import numpy as np
 import pickle
 
+
 # Load the trained model
 model = pickle.load(open("model.pkl", "rb"))
 
 # App UI
+from PIL import Image
+image = Image.open("logo.png")
+
 st.set_page_config(page_title="🐔 Chicken Weight Predictor", layout="centered")
 st.title("🐔 Chicken Weight Predictor")
 st.markdown("Enter chicken height in cm to predict its weight (kg).")
